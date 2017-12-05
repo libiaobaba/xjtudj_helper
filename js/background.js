5416be5d6307eb0c25a6fc10246295b6d8f19bea
xczh <xczh.me@foxmail.com>
2017-12-05 21:45:33 +0800

    add background.html

--- js/background.js
+++ js/background.js
@@ -9,8 +9,9 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 // cancel course_updateUserWatchRecord ajax request to server for safety
 chrome.webRequest.onBeforeRequest.addListener(
     function(details){
-        console.log('[%s] canceled xmlhttprequest: %o', logPrefix, details);
-        return {cancel: true};
+        console.log('[%s] redirected xmlhttprequest: %o', logPrefix, details);
+        content = '{"status":100}';
+        return {redirectUrl: "data:application/json;charset=UTF-8," + content};
     },
     {
         urls: [
