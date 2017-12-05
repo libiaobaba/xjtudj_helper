8aeda7cdda99304059c05f24e846ec3c7c732136
xczh <xczh.me@foxmail.com>
2017-12-05 13:45:05 +0800

    block xmlhttprequest for updateWatchRecord

--- js/background.js
+++ js/background.js
@@ -1,6 +1,26 @@
+let logPrefix = 'background.js'
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg == 'popup') {
         sendResponse('emmm... I\'m background.');
     }
 });
+
+// cancel course_updateUserWatchRecord ajax request to server for safety
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details){
+        console.log('[%s] canceled xmlhttprequest: %o', logPrefix, details);
+        return {cancel: true};
+    },
+    {
+        urls: [
+            "*://xjtudj.edu.cn/course/course_updateUserWatchRecord.do*",
+        ],
+        types: [
+            "xmlhttprequest",
+        ]
+    },
+    [
+        "blocking"
+    ]
+);
